5f392d56f3eeddbecfbaed56d686c1c9f524fe68
mehmet <mehmet-ulker@hotmail.com>
2025-06-27 16:05:44 +0300

    Rename index.js to server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 import connectDB from "./lib/db.js";
+import protectRoute from "./middleware/protecRoute.js";
 dotenv.config();
 const app = express();
+app.use(express.json()); //
+app.use(cookieParser());
 
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", protectRoute, messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
